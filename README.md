a0fa490d2ba058195b58fc9268cc55f1d4c23399
kunalphalswal <111356016+kunalphalswal@users.noreply.github.com>
2022-09-26 22:46:36 +0530

    added a notes file
    added basic notes to it

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #Demo
 
 Some description.
+
+##subheader
+
+Learning tutorial from freecodecamp.
---
+++ illustration.cpp
@@ -0,0 +1,11 @@
+//we need to tell git to track the file before we can save it.(both for a new file and for changes made to an existing file)
+//using git add . (to track all the listed files) or git add <filename> to track that specific file/folder.
+//to commit changed to git use: git commit -m "(for main message, it's compulsory)" -m ""(for extended description)
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"hello world.";
+    return 0;
+}
