7dff8b11a5b78d407ec3eebf43b7b968ef13c6a0
kunalphalswal <111356016+kunalphalswal@users.noreply.github.com>
2022-09-26 22:48:48 +0530

    added about commit

--- illustration.cpp
+++ illustration.cpp
@@ -1,7 +1,7 @@
 //we need to tell git to track the file before we can save it.(both for a new file and for changes made to an existing file)
 //using git add . (to track all the listed files) or git add <filename> to track that specific file/folder.
 //to commit changed to git use: git commit -m "(for main message, it's compulsory)" -m ""(for extended description)
-
+//until now we have only saved our changes locally, the commit still isn't live yet.
 
 #include<bits/stdc++.h>
 using namespace std;
